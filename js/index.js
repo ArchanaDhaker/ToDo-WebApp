72ea10495ee0128866299ca38ee3ffc018a700c3
Archana Dhaker <archana2561996@gmail.com>
2020-09-06 22:34:10 +0530

    Latest

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".ItemButton").click(function(){
-      $(".listing").append("<div class='NewValues'>"+" <div><img src='assets/check_box_outline_blank-black-18dp.svg'></div>"+ $(".inputBox").val()+" <div><img src='assets/edit-black-18dp.svg'><img src='assets/delete-black-18dp.svg'></div>" +"</div>");
+      $(".listing").append("<div class='NewValues'>"+" <div class='checkBox'><img src='assets/check_box_outline_blank-black-18dp.svg'></div>"+ $(".inputBox").val()+" <div class='edit'><img src='assets/edit-black-18dp.svg'><img src='assets/delete-black-18dp.svg'></div>" +"</div>");
     });
-    
+
   });
+

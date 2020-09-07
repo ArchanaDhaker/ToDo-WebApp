e7f8592c9f816fda67e7678a2beb51ab37b63756
Archana Dhaker <archana2561996@gmail.com>
2020-09-07 22:16:47 +0530

    New files

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $(".ItemButton").click(function(){
       $(".listing").append("<div class='NewValues'>"+" <div class='checkBox'><img src='assets/check_box_outline_blank-black-18dp.svg'></div>"+ $(".inputBox").val()+" <div class='edit'><img src='assets/edit-black-18dp.svg'><img src='assets/delete-black-18dp.svg'></div>" +"</div>");
+    
     });
-
+    
+  
   });
 

c2699f3b0f49d03ab10fc96329303ef54e0558c2
Archana Dhaker <archana2561996@gmail.com>
2020-09-11 23:05:32 +0530

    US attack

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    
+    $("#all").click(function(){
+        $(".active").removeClass("active");
+        $("#all").addClass("active"); 
+    }); 
+    $("#pending").click(function(){
+        $(".active").removeClass("active");
+        $("#pending").addClass("active"); 
+    });  
+    $("#completed").click(function(){
+        $(".active").removeClass("active");
+        $("#completed").addClass("active"); 
+    }); 
   
 });
 

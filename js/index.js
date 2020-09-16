afe4d9ac85df1eff36dddc522f69d60e35e0fac9
Archana Dhaker <archana2561996@gmail.com>
2020-09-16 23:01:36 +0530

    16 Sept

--- js/index.js
+++ js/index.js
@@ -3,21 +3,24 @@ $(document).ready(function(){
         $(".active").removeClass("active");
         $("#all").addClass("active"); 
     }); 
+
     $("#pending").click(function(){
         $(".active").removeClass("active");
         $("#pending").addClass("active"); 
-    });  
+    }); 
+     
     $("#completed").click(function(){
         $(".active").removeClass("active");
         $("#completed").addClass("active"); 
     }); 
 
     $("#plus").click(function(){
-        console.log("cahercckhinang");
+        console.log("checking plus");
         $(".templet_1").addClass("hide");
         $(".templet_2").removeClass("hide"); 
         $("#right").addClass("hide");
         $("#direction").removeClass("hide");
+        //$("#plus").addClass("hide");
     }); 
 
     $("#direction").click(function(){
@@ -27,4 +30,10 @@ $(document).ready(function(){
         $("#right").removeClass("hide");
         $(".templet_2").addClass("hide");
     });
+
+    $(".imgBox").click(function(){
+        // yaha ander krna hai
+        // wo input div g box ke ander hai isliye imgbox pe click hone pe ek loop ban raha hai bahar nikaal de us div ko
+        $('#imgFile').trigger("click");
+    })
 });
